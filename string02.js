fbc43d7b418bd1f2ef4a5e76a5cc78d3d722ab1a
Vrudhi Pandey <vrudhipandey@gmail.com>
2026-06-25 15:11:53 +0530

    add JS string02.js

---
+++ string02.js
@@ -0,0 +1,30 @@
+const name = "khushi"
+const repocount = 50
+
+console.log(name + repocount)//outdate to combine 
+
+console.log(`hello my name is ${name} and repo count is ${repocount}`)//backstciks its modern way to write the syntax
+const gamename = ('khu-shi-pa-ndey')//declaring the string 
+console.log(gamename [0])
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());//method
+console.log(gamename.charAt(5));// method 
+// console.log(gamename.indexof('i'));
+
+const namee=gamename.substring(0,4)
+console.log(namee)//this method cut thr string (we cannot use negative value)
+
+const nameee= gamename.slice(0,-4)
+console.log(nameee)//method (we can use negative value )
+
+const stringname = '    cat   '
+console.log(stringname)
+console.log(stringname.trim())// its remove the spacee 
+
+const url ="https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))//method
+
+console.log(url.includes('sundar'))//method
+console.log(gamename.split('-'))//method
+
