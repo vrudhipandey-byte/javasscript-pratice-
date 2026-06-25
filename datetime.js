3436e3d9d9b5fa83542fbc00f58e929611859b99
Vrudhi Pandey <vrudhipandey@gmail.com>
2026-06-25 16:43:09 +0530

    add JS: datetime.js

---
+++ datetime.js
@@ -0,0 +1,10 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(typeof mydate);
+
+let mycreatedate = new Date(2026,8,31);
+console.log(mycreatedate);
+console.log(mycreatedate.toISOString());
